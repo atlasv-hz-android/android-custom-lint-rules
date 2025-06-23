de5c74ae88a13752beafcdde7bf37fa26b02dd46
weiping <weiping@atlasv.com>
2025-06-23 18:15:18 +0800

    feat: Update Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.gradle.kotlin.dsl.withType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
@@ -5,9 +8,26 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.lint) apply false
+    alias(libs.plugins.versionscheck)
 }
 buildscript {
     dependencies {
         classpath(libs.publishlib)
     }
+}
+
+// 检测依赖更新 https://github.com/ben-manes/gradle-versions-plugin
+// ./gradlew dependencyUpdates
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
